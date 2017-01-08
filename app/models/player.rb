3339d27bf368d80ea985f77f2ae254c90d75114a
Jonathan Gray <jonathanceddy@gmail.com>
2017-01-07 23:19:49 -0800

    unrequired passwords

--- app/models/player.rb
+++ app/models/player.rb
@@ -14,4 +14,10 @@ class Player < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable
+
+  protected
+
+  def password_required?
+    false
+  end
 end
