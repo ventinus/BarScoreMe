4d15bfc80ba76a0ccfc8c50e2371963ff4cf4a30
Jonathan Gray <jonathanceddy@gmail.com>
2017-01-07 21:53:34 -0800

    setting up players with devise

---
+++ app/controllers/players_controller.rb
@@ -0,0 +1,54 @@
+class PlayersController < ApplicationController
+  before_action :set_player, only: [:edit, :update, :destroy]
+
+  def index
+    @players = Player.all
+  end
+
+  def edit
+  end
+
+  def new
+    @player = Player.new
+  end
+
+  def create
+    @player = Player.new(player_params)
+
+    if @player.save
+      redirect_to admin_administrators_path, notice: 'Player was successfully created.'
+    else
+      flash[:error] = 'Player was not created.'
+      render :new
+    end
+  end
+
+  def update
+    if @player.update_with_password(player_params)
+      redirect_to admin_players_path, notice: 'Player was successfully updated.'
+    else
+      flash[:error] = 'Player was not updated.'
+      render :edit
+    end
+  end
+
+  def destroy
+    if @player.destroy
+      redirect_to admin_players_path, notice: "#{@player.full_name} was removed as an player."
+    else
+      flash[:error] = "Player was not deleted. #{@player.errors.full_messages.to_sentence}."
+      redirect_to admin_players_path
+    end
+  end
+
+  private
+
+  def set_player
+    @player = Player.find(params[:id])
+  end
+
+  def player_params
+    params[:player].permit(:email, :name, :current_password,
+                            :password, :password_confirmation)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   devise_for :players
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # get '/', to: redirect('/players/sign_in')
+
+  resources :players, except: :show
 end
